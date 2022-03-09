6ff203abb97fc91ef590eb5c2b64a5e1fc770a8a
seongho-joo <hazzyseong@gmail.com>
2022-03-09 18:58:37 +0900

    Chore: 조회한 빈이 모두 필요할 때 - Map, List

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -4,10 +4,11 @@ import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@MainDiscountPolicy
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private final int discountPercent = 10;
